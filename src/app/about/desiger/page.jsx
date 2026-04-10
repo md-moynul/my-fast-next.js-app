3ada93ddd3adfdd170e63ae913caeb37434e3f86
moynul447 <mmmdmoynulislam@gmail.com>
2026-04-10 16:02:06 +0600

    router explore

---
+++ src/app/about/desiger/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DesignerPage = () => {
+    return (
+        <div>
+            <h1>designer page</h1>
+        </div>
+    );
+};
+
+export default DesignerPage;
---
+++ src/app/contact/page.jsx
@@ -0,0 +1,11 @@
+
+
+const ContactPage = () => {
+    return (
+        <div>
+            <h1 className="text-3xl text-semibold text-center">Contact pages here</h1>
+        </div>
+    );
+};
+
+export default ContactPage;
